464ead59e759da6ed46747f3a791fd79cd4a5fcc
Kreik2045 <angryangeli.agcc@gmail.com>
2025-09-01 10:22:41 -0600

    Entregable 6-Dataframe config

--- Entregable 2/rapidfuzz_tables_app.py
+++ Entregable 2/rapidfuzz_tables_app.py
@@ -1,4 +1,5 @@
 from rapidfuzz_table_app_2 import execute_dynamic_matching
+import pandas as pd 
 
 params_dict = {
     "server": "localhost",
@@ -18,4 +19,5 @@ params_dict = {
 
 resultados = execute_dynamic_matching(params_dict, score_cutoff=70)
 matches_filtrados = [r for r in resultados if r.get('score', 0) > 70]  
-print(matches_filtrados)
+df = pd.DataFrame(matches_filtrados)
+print(df)
